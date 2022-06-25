bf8b99f1b490328b986ae65f857c5faa98b51ce5
JannyferTamagmo <jannyfertamagno@gmail.com>
2022-06-25 16:45:43 -0300

    subindo o backend

---
+++ src/routes.ts
@@ -0,0 +1,71 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+//-- ROTAS USER
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+
+//-- ROTAS CATEGORY
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+//-- ROTAS PRODUCT
+import { CreateProductController } from './controllers/product/CreateProductController'
+import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+
+//-- ROTAS ORDER
+import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
+import { SendOrderController } from './controllers/order/SendOrderController'
+
+import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
+
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+import uploadConfig from './config/multer'
+
+const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
+
+//-- ROTAS USER
+router.post('/users', new CreateUserController().handle)
+
+router.post('/login', new AuthUserController().handle)
+
+router.get('/info', isAuthenticated,  new DetailUserController().handle )
+
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle )
+
+//-- ROTAS ORDER
+router.post('/order', isAuthenticated, new CreateOrderController().handle )
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle )
+
+router.post('/order/additem', isAuthenticated, new AddItemController().handle )
+router.delete('/order/removeitem', isAuthenticated, new RemoveItemController().handle )
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+
+router.get('/orders', isAuthenticated, new ListOrdersController().handle )
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
+
+
+
+export { router }; 
